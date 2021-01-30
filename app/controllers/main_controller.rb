99bb76bac47aef9f0a724f1a5792831a6ef0a0c3
andesdevroot <andes.dev.root@gmail.com>
2021-01-30 15:22:32 -0300

    se crean messages flash

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,10 @@
 class MainController < ApplicationController
     def  index
-        
+        flash.now[:notice] = "Ingreso exitoso"
+        flash.now[:alert] = "Email o password inválido"
     end
 end
+
+
+
 
