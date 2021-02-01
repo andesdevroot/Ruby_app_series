e875860a837985f5ce58ee404ca822159875e015
andesdevroot <andes.dev.root@gmail.com>
2021-01-31 21:46:17 -0300

    se crea formulario para registro de usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
 
   get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registration#create"
 
   root to: "main#index"
 end
+
+
 
 
 
