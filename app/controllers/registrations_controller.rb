e8686c4cad811f8707cd1f8d9f0575774f42ffef
andesdevroot <andes.dev.root@gmail.com>
2021-02-05 13:36:45 -0300

    update form error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,24 @@ def new
     @user = User.new
 end
 
+def create
+    @user = User.new(user_params)
+    if @user.save
+        redirect_to root_path, notice: "Cuenta creada con éxito"
+    else
+        render :new
+    end
 end
+
+private
+
+def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+end
+
+end
+
+
+
+
+
